503b14c84255f1a1d675c15572ab7b39722b6b17
James Little <alphafoobar@users.noreply.github.com>
2019-05-21 21:48:11 +1200

    Logging can be used
    ```
    (log/info "something" x "by" y)
    ```

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clojure "0.1.0-SNAPSHOT"
   :description "Code Kata - practicing better programming"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"],
+                 [org.clojure/tools.logging "0.4.1"]
+                 ]
   :main ^:skip-aot kata02-binary-search.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
